00bda6053e8e6237281f378d4a61449d4125e3b0
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-10-13 18:19:41 +0700

    feat: add skeleton for components and scenes

---
+++ src/scenes/global/Sidebar.tsx
@@ -0,0 +1,9 @@
+import { JSX } from "react";
+
+export default function Sidebar(): JSX.Element {
+  return (
+    <div>
+      Sidebar
+    </div>
+  );
+}
---
+++ src/scenes/global/Topbar.tsx
@@ -0,0 +1,9 @@
+import { JSX } from "react";
+
+export default function Topbar(): JSX.Element {
+  return (
+    <div>
+      Topbar
+    </div>
+  );
+}
