e64c5e1b68e7b1e8e91a4470631f93d7c36cfcd2
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-10-16 21:30:02 +0700

    feat: add context provider and theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { JSX } from "react";
+import { ColorModeContext, useMode } from "./theme";
+import { ThemeProvider } from "@mui/material";
 
 export default function App(): JSX.Element {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <div className="app">
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
