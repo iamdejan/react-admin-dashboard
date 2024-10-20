3532eb42e7c01825ebf56eaacf3bb872764a986a
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-10-20 15:48:14 +0700

    feat: add CSS Baseline and main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { JSX } from "react";
 import { ColorModeContext, useMode } from "./theme";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 export default function App(): JSX.Element {
   const [theme, colorMode] = useMode();
@@ -8,7 +8,9 @@ export default function App(): JSX.Element {
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <div className="app">
+          <main className="content"></main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
