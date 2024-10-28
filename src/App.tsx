1c1245891d6a72b8ad25c7592d81c292efd9232f
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-10-28 14:42:13 +0700

    feat: add FAQ page

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Calendar from "./scenes/calendar";
+import FAQ from "./scenes/faq";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import { Route, Routes } from "react-router-dom";
 
@@ -32,13 +32,13 @@ export default function App(): JSX.Element {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
-              <Route path="/calendar" element={<Calendar />} />
             </Routes>
           </main>
         </div>
