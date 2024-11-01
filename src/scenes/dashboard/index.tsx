9814b524a9ca9167a09039c1a88b0a2153c0f53e
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-11-01 14:24:21 +0700

    feat: add Download Report button

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,12 +1,36 @@
 import { JSX } from "react";
 import Header from "../../components/Header";
-import { Box } from "@mui/material";
+import { Box, Button, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
 
 export default function Dashboard(): JSX.Element {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subTitle="Welcome to your dashboard!" />
+      </Box>
+
+      <Box>
+        <Button
+          sx={{
+            backgroundColor: colors.blueAccent[700],
+            color: colors.grey[100],
+            fontSize: "14px",
+            fontWeight: "bold",
+            padding: "10px 20px",
+          }}
+        >
+          <DownloadOutlinedIcon
+            sx={{
+              mr: "10px"
+            }}
+          />
+          Download Report
+        </Button>
       </Box>
     </Box>
   );
