6345a149859c99942d9f58f1ea54f610084bc091
Giovanni Dejan <15686093+iamdejan@users.noreply.github.com>
2024-11-01 16:38:26 +0700

    feat: enrich Dashboard page (#8)

---
+++ src/components/StatBox.tsx
@@ -0,0 +1,53 @@
+import { JSX } from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+import ProgressCircle from "./ProgressCircle";
+
+type StatBoxProps = {
+  title: string,
+  subTitle: string,
+  icon: JSX.Element,
+  progress: number,
+  increase: string
+};
+
+export default function StatBox({title, subTitle, icon, progress, increase}: StatBoxProps): JSX.Element {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box width="100%" m="0 30px">
+      <Box display="flex" justifyContent="space-between">
+        <Box>
+          {icon}
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            sx={{color: colors.grey[100]}}
+          >
+            {title}
+          </Typography>
+        </Box>
+        <Box>
+          <ProgressCircle progress={progress} />
+        </Box>
+      </Box>
+
+      <Box display="flex" justifyContent="space-between" mt="2px">
+        <Typography
+          variant="h5"
+          sx={{color: colors.greenAccent[500]}}
+        >
+          {subTitle}
+        </Typography>
+        <Typography
+          variant="h5"
+          fontStyle="italic"
+          sx={{color: colors.greenAccent[600]}}
+        >
+          {increase}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
